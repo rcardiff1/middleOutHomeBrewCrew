10986a1bd84c5316c76a455d6e870c7704f96766
rcardiff1 <robertstuartcardiff@gmail.com>
2016-02-11 18:18:17 -0800

    [refactor] added facebook callbackURL to fbroute.js

--- server/fbroute.js
+++ server/fbroute.js
@@ -1,7 +1,21 @@
-module.exports = {
-  'facebookAuth' : {
-    'clientID'      : 1677657699169186, // your App ID
-    'clientSecret'  : '95b9cf6f8f9739bec2dfe5c275c61719', // your App Secret
-    'callbackURL'   : 'http://localhost:4000/auth/facebook/callback'
-  }
-};
+if(process.env.DEPLOYED) {
+	module.exports = {
+	 'facebookAuth' : {
+	   'clientID'      : 1677657699169186, // your App ID
+	   'clientSecret'  : '95b9cf6f8f9739bec2dfe5c275c61719', // your App Secret
+	   'callbackURL'   : 'https://socialflixplayer.herokuapp.com/auth/facebook/callback'
+	 }
+	};
+} else {
+	module.exports = {
+	  'facebookAuth' : {
+	    'clientID'      : 1677657699169186, // your App ID
+	    'clientSecret'  : '95b9cf6f8f9739bec2dfe5c275c61719', // your App Secret
+	    'callbackURL'   : 'http://localhost:4000/auth/facebook/callback'
+	  }
+	};
+}
+
+
+
+
